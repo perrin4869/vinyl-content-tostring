7c626615602fcfd9c57e9d1729e5e1017805d279
Julian Grinblat <julian@dotcore.co.il>
2017-10-23 10:06:41 +0900

    Support node 4 in .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,8 @@
 const env = process.env.NODE_ENV || 'production';
 
+const plugins = env === 'test' ? ['istanbul'] : [];
+
 module.exports = {
   presets: [['env', { targets: { node: 4 } }]],
-  plugins: [...(env === 'test' ? ['istanbul'] : [])],
+  plugins,
 };
