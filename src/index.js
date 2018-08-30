690e0ae513c5f966395e34f8fc41316d0d7d7633
Julian Grinblat <julian@dotcore.co.il>
2018-08-31 01:14:48 +0900

    Upgrade eslint

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ import streamToString from 'stream-to-string';
 
 module.exports = (file, enc) => {
   if (
-    !file ||
-    typeof file.isBuffer !== 'function' ||
-    typeof file.isStream !== 'function'
+    !file
+    || typeof file.isBuffer !== 'function'
+    || typeof file.isStream !== 'function'
   ) {
     return Promise.reject(new TypeError('First argument must be a Vinyl file'));
   }
 
   if (file.isBuffer()) {
     return Promise.resolve(file.contents.toString(enc));
-  } else if (file.isStream()) {
+  }
+
+  if (file.isStream()) {
     // TODO: support encoding in streamToString
     return streamToString(file.contents, enc);
   }
